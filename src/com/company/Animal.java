0e982c2248513e838af2e696c0f035e894ee2e1e
maleckyy <kupferpatryk@gmail.com>
2020-05-02 19:03:53 +0200

    karmienie i wyprowadzanie zwierzaka

--- src/com/company/Animal.java
+++ src/com/company/Animal.java
@@ -1,4 +1,28 @@
 package com.company;
 
 public class Animal {
+    String species;
+    private Double weight;
+
+    public Animal() {
+
+        this.species = species;
+        this.weight = 10.0;
+    }
+
+
+    void feed(){
+        weight++;
+        System.out.println("psinka zjadła i waży"+" "+weight);
+    }
+    void takeForAWalk(){
+        if(weight>1){
+            weight--;
+            System.out.println("spacer udany i ja psinka waże"+" "+weight);
+        }else{
+            System.out.println("ja pies nie żyje...");
+        }
+    }
+
+
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,23 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        Animal animal = new Animal();
+        animal.feed();
+        animal.feed();
+        animal.takeForAWalk();
+        animal.takeForAWalk();
+        animal.takeForAWalk();
+        animal.takeForAWalk();
+        animal.takeForAWalk();
+        animal.takeForAWalk();
+        animal.takeForAWalk();
+        animal.takeForAWalk();
+        animal.takeForAWalk();
+        animal.takeForAWalk();
+        animal.takeForAWalk();
+        animal.takeForAWalk();
+        animal.takeForAWalk();
+
 
     }
 }
