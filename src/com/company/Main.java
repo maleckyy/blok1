c129ad8aaa175ae7df6198bb768e408d3704279a
maleckyy <kupferpatryk@gmail.com>
2020-05-17 17:33:51 +0200

    7 klasa device

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,6 +1,7 @@
 package com.company;
 
 import com.company.devices.Car;
+import com.company.devices.Device;
 import com.company.devices.Phone;
 
 public class Main {
@@ -23,38 +24,38 @@ public class Main {
         animal.takeForAWalk();
         animal.takeForAWalk();
 
-        Phone myphone= new Phone();
-        myphone.number="857798947";
-        myphone.producer="huawei";
+        Phone myphone = new Phone();
+        myphone.number = "857798947";
+        myphone.producer = "huawei";
 
-        Human human1= new Human();
-        human1.fName="Patryk";
-        human1.lName="Kupfer";
+        Human human1 = new Human();
+        human1.fName = "Patryk";
+        human1.lName = "Kupfer";
         human1.setSalary(3000.23D);
 
-        Animal dog= new Animal();
-        dog.petName="pies";
-        human1.pet= dog;
+        Animal dog = new Animal();
+        dog.petName = "pies";
+        human1.pet = dog;
 
-        Car myCar= new Car();
+        Car myCar = new Car();
         myCar.setModel("v40");
         myCar.setProducer("Volvo");
         myCar.setValue(12000D);
         human1.addCar(myCar);
 
-        Car newCar=new Car();
+        Car newCar = new Car();
         newCar.setModel("v60");
         newCar.setProducer("Volvo");
         newCar.setValue(18000.50D);
-        boolean equal = newCar.getModel().equals(myCar.getModel())&& newCar.getProducer().equals(myCar.getProducer());
+        boolean equal = newCar.getModel().equals(myCar.getModel()) && newCar.getProducer().equals(myCar.getProducer());
         System.out.println(equal);//porównanie czy są takie same, są false bo mają rozne wartosci w modelu
 
 
-        Car newCar1=new Car();
+        Car newCar1 = new Car();
         newCar1.setModel("v60");
         newCar1.setProducer("Volvo");
         newCar1.setValue(18000.50D);
-        boolean equal1 = newCar1.getModel().equals(newCar.getModel())&& newCar1.getProducer().equals(newCar.getProducer());
+        boolean equal1 = newCar1.getModel().equals(newCar.getModel()) && newCar1.getProducer().equals(newCar.getProducer());
         System.out.println(equal1);//true poniewaz są takie same
 
         System.out.println(newCar);
@@ -63,5 +64,17 @@ public class Main {
         System.out.println(human1.toString());
         System.out.println(dog.toString());
         System.out.println(myphone.toString());
+
+        Device device = new Device() {
+
+            @Override
+            public boolean tunrOn() {
+                System.out.println("włączone");
+                return true;
+            }
+
+        };
+        device.tunrOn();
+        System.out.println(device.toString());
     }
 }
--- src/com/company/devices/Phone.java
+++ src/com/company/devices/Phone.java
@@ -1,10 +1,16 @@
 package com.company.devices;
 
-public class Phone {
+public class Phone extends Device{
     public String number;
     public String producer;
 
     public String toString(){
         return producer+" o numerze "+number;
+    }
+
+    @Override
+    public boolean tunrOn() {
+        System.out.println("telefon jest włączony");
+        return true;
     }
 }
